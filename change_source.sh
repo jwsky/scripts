d826c3a6afda6924f7efbc1cf235e25839f283be
jwsky <jiangweiluo@gmail.com>
2024-08-12 16:52:02 +0800

    Update change_source.sh

--- change_source.sh
+++ change_source.sh
@@ -4,6 +4,7 @@
 TUNA_SOURCE="http://mirrors.tuna.tsinghua.edu.cn/ubuntu/"
 USTC_SOURCE="http://mirrors.ustc.edu.cn/ubuntu/"
 ALIBABA_SOURCE="http://mirrors.aliyun.com/ubuntu/"
+ALIBABA_INTERNAL_SOURCE="http://mirrors.cloud.aliyuncs.com/ubuntu/"
 DEFAULT_SOURCE="http://archive.ubuntu.com/ubuntu/"
 
 # 定义备份文件路径
@@ -18,8 +19,9 @@ echo "请选择要使用的软件源："
 echo "1) 清华源"
 echo "2) 中科大源"
 echo "3) 阿里云源"
-echo "4) Ubuntu 默认源"
-read -p "请输入对应的数字 [1-4]: " source_choice
+echo "4) 阿里云内网源（仅适用于阿里云 ECS）"
+echo "5) Ubuntu 默认源"
+read -p "请输入对应的数字 [1-5]: " source_choice
 
 # 根据用户选择更新 sources.list
 case $source_choice in
@@ -33,6 +35,9 @@ case $source_choice in
         SELECTED_SOURCE=$ALIBABA_SOURCE
         ;;
     4)
+        SELECTED_SOURCE=$ALIBABA_INTERNAL_SOURCE
+        ;;
+    5)
         SELECTED_SOURCE=$DEFAULT_SOURCE
         ;;
     *)
