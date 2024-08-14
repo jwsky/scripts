c61309b8542e12d18aa612bd84a04c1eeb1ea9f4
jwsky <jiangweiluo@gmail.com>
2024-08-14 10:28:36 +0800

    Update rinetd.sh

--- rinetd.sh
+++ rinetd.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+# 检查是否在bash下运行
+if [ -z "$BASH_VERSION" ]; then
+  echo "脚本未在bash下运行，正在切换到bash..."
+  exec /bin/bash "$0" "$@"
+fi
+
 # 检查是否以root用户运行脚本
 if [ "$EUID" -ne 0 ]; then
   echo "请以root用户权限运行此脚本。"
   exit 1
 fi
 
-# 检查是否已安装rinetd，通过判断返回结果中是否包含 'inetd' 字符串
-if ! command -v rinetd | grep -q "inetd"; then
+# 检查是否已安装rinetd，通过判断返回结果中是否包含 'rinetd' 字符串
+if ! command -v rinetd | grep -q "rinetd"; then
   echo "rinetd未安装，现在安装rinetd..."
   sudo apt update
   sudo apt install -y rinetd
 else
   echo "rinetd已安装，继续执行..."
 fi
-
 
 # 下载加密的配置文件
 config_url="https://s.theucd.com/file/rinetd.conf.enc"
@@ -23,8 +28,8 @@ wget -O "$config_file" "$config_url"
 
 # 提示输入解密密码，并明确等待用户输入
 echo
-echo "请输入解密密码: "
-read -s decrypt_password
+read -p "请输入解密密码: " -s decrypt_password
+echo
 
 # 解密并替换/etc/rinetd.conf
 openssl enc -d -aes-256-cbc -in "$config_file" -out /etc/rinetd.conf -k "$decrypt_password" -md sha256 -pbkdf2 -iter 100000
