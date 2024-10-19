8f5a816d3bb849747f94c1ff9aa663fd58062f82
jwsky <jiangweiluo@gmail.com>
2024-10-20 00:19:36 +0800

    Update caps.sh

--- caps.sh
+++ caps.sh
@@ -33,7 +33,7 @@ case $choice in
 
     # 7. 安装依赖，使用清华源
     apt install python3-pip -y
-    pip3 install -r capswriter/requirements-server.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+    pip3 install -r capswriter/requirements-server.txt -i https://pypi.tuna.tsinghua.edu.cn/simple --break-system-packages
 
     # 8. 创建 systemd 服务文件以在开机时自动启动
     echo "创建 systemd 服务文件..."
